0340182dcd82500f22d44bef1bc82a3106838e3a
STU-Chi <s22115103@stu.edu.tw>
2024-10-16 11:31:27 +0800

    V1.2

--- ch1.js
+++ ch1.js
@@ -89,7 +89,7 @@ function isBinary(str) {
 function textToBinary(text) {
     return text.split('')
         .map(char => char.charCodeAt(0).toString(2).padStart(8, '0'))
-        .join('');
+        .join(' ');
 }
 
 function binaryToText(binary) {
